14d67a4f908439032340170aba004e66de14c273
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:26:26 -0500

    teqc splice v1 local save

--- teqc/splice/teqc_splice.sh
+++ teqc/splice/teqc_splice.sh
@@ -1,26 +1,29 @@
 #!bin/bash
 # a script to splice all rinex files in a directory and output to the same directory
 # also adds in marker name (first input)
-# assumes antenna name is already in rinex (from sbf converter output)
+# also converts to ...
 
 teqc='/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc'
-dir_in='/mnt/f/PAD2019/GNSS/GNSS_ALL_PAD19_RINEX2.11/'
-for g in "$dir_in"*/ # g directory for collection day
-do
-	echo "directory: "$g
-	date=$(echo $g | awk -F'/' '{print $(NF-1) }') #common date, not DOY
+dir_in=$(pwd)
+	echo "directory: "$dir_in
+	date=$(echo $dir_in | awk -F'/' '{print $7 }') #common date, not DOY
 	#echo $date
-	h=$(ls -t "$g"*.19O | tail -1) # sample file ID base name (oldest file in dir.)
-	j=$(basename $h) # take only local path
+	h=$(ls -t "$dir_in"/GPS_only/*.dat.rnx | tail -1) # sample file ID base name (oldest file in dir.)
+	j=$(echo $h | awk -F'/' '{print $9 }') # take only local path
 	#echo "J="$j
-	fileBase="${j:0:9}" # clip to just YYYYMMDD numerals
+	fileBase="${j:0:9}" 
 	#echo $h
-	spliceName=$(echo $fileBase"_"$date"D.19O")
+	spliceName=$(echo $fileBase"_"$date"D.dat")
+	echo $sp
 	rm $g$spliceName # remove existing file before splicing!
+	mkdir "$dir_in"/GPS_only/
+        for j in LOG*.dat
+        do
+                $teqc -O.s G -R $j > "$dir_in"/GPS_only/$j.rnx
+        done
 	echo "Creating file: "$spliceName
-	$teqc -O.mo "\""$1"\"" $g*.19O > $g$spliceName
+	$teqc -O.at SEPPOLANT_X_MF -O.mo "\""$1"\"" $g*.dat > $g$spliceName
 	#compressedName=${spliceName:0:19}".tar"
 	#echo "Compressing: "$compressedName
 	#tar -czf $g$compressedName $g$spliceName
 	echo ""
-done
