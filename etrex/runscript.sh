5dc2ac1e7ef30fabfdd7a4e01f770741b233f20e
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 10:21:02 -0500

    add runscript

---
+++ etrex/runscript.sh
@@ -0,0 +1,30 @@
+# for batch running
+# EXAMPLE: gpx2shp_4.sh all /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/REPLACE/ REPLACE
+
+# gpx2shp_4.sh all /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/tracks/ tracks
+# gpx2shp_4.sh all /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/track_points/ track_points
+# gpx2shp_4.sh all /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/waypoints/ waypoints
+# gpx2shp_4.sh archive /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/tracks/ tracks
+# gpx2shp_4.sh archive /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/track_points/ track_points
+# gpx2shp_4.sh archive /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp/waypoints/ waypoints
+
+## PAD 2018
+# gpx2shp_4.sh UCLA1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tracks/ tracks
+# gpx2shp_4.sh UCLA1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/track_points/ track_points
+# gpx2shp_4.sh UCLA1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/waypoints/ waypoints
+#
+# gpx2shp_4.sh UCLA2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tracks/ tracks # HERE
+# gpx2shp_4.sh UCLA2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/track_points/ track_points
+gpx2shp_4.sh UCLA2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/waypoints/ waypoints
+
+# gpx2shp_4.sh UMass /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tracks/ tracks
+# gpx2shp_4.sh UMass /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/track_points/ track_points
+# gpx2shp_4.sh UMass /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/waypoints/ waypoints
+#
+# gpx2shp_4.sh UNC1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tracks/ tracks
+# gpx2shp_4.sh UNC1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tratrack_pointscks/ track_points
+# gpx2shp_4.sh UNC1 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/waypoints/ waypoints
+#
+# gpx2shp_4.sh UNC2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/tracks/ tracks
+# gpx2shp_4.sh UNC2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/track_points/ track_points
+# gpx2shp_4.sh UNC2 /mnt/f/PAD2018/etrex_data/raw/ /mnt/f/PAD2018/etrex_data/shp/waypoints/ waypoints
