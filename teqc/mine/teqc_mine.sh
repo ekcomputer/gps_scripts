535c1d54ed923646668fbc15fbde0074f0cccfb1
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:21:12 -0500

    teqc mine v4

--- teqc/mine/teqc_mine.sh
+++ teqc/mine/teqc_mine.sh
@@ -9,22 +9,22 @@ echo $logs
 for f in "$searchpath"/rinex/*/*/*/*.dat
 do
 	echo "Processing: ""$f"
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep filename | awk -F':' '{print $2}' >> "$logs"/filename.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep bytes | awk -F':' '{print $2}' >> "$logs"/bytes.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep start | awk -F' ' '{print $5" "$6}' >> "$logs"/start.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep final | awk -F' ' '{print $5" "$6}' >> "$logs"/stop.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep sample | awk -F':' '{print $2}' >> "$logs"/sample.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep possible | awk -F':' '{print $2}' >> "$logs"/possibleMissingEpochs.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep name | awk -F':' '{print $2}' >> "$logs"/stationName.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep code | awk -F':' '{print $2}' >> "$logs"/stationCode.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep latitude | awk -F':' '{print $2}' >> "$logs"/lat.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep longitude | awk -F':' '{print $2}' >> "$logs"/long.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep elevation | awk -F':' '{print $2}' >> "$logs"/elev.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "satellites w/ obs" | awk -F':' '{print $2}' >> "$logs"/sats.txt
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "Complete observations" | awk -F':' '{print $2}' >> "$logs"/completeObs.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep filename | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/filename.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep bytes | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/bytes.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep start | awk -F' ' '{gsub(/ /, "", $5); gsub(/ /, "", $6); print $5" "$6}' >> "$logs"/start.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep final | awk -F' ' '{gsub(/ /, "", $5); print $5" "$6}' >> "$logs"/stop.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep sample | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/sample.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep possible | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/possibleMissingEpochs.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep "station name:" | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/stationName.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep code | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/stationCode.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep latitude | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/lat.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep longitude | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/long.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep elevation | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/elev.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "satellites w/ obs" | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/sats.txt
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "Complete observations" | awk -F':' '{gsub(/ /, "", $2); print $2}' >> "$logs"/completeObs.txt
 
 
 done
-
+echo "Finished batch mining."
 
 
