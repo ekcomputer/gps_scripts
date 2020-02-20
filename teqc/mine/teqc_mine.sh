9e3f7fffc65ba3d4d8f29a891f9d4e0ea0b6e256
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:20:06 -0500

    teqc mine v2

--- teqc/mine/teqc_mine.sh
+++ teqc/mine/teqc_mine.sh
@@ -5,20 +5,20 @@ searchpath="$1" # name of directory w data
 echo -e "Starting mining script for: ""$1""\n\n"
 for f in "$searchpath"/rinex/*/*/*/*.dat
 do
-	echo "Processing: ""$f"
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep filename | awk -F':' '{print $2}' 
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep bytes | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep start | awk -F':' '{print $2" "$3}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep stop | awk -F':' '{print $2" "$3}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep sample | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep possible | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep name | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep code | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep latitude | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep longitude | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" | grep elevation | awk -F':' '{print $2}'
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" | grep "satellites w/ obs" | awk -F':' '{print $2}'
-
+#	echo "Processing: ""$f"
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep filename | awk -F':' '{print $2}' 2> /dev/null 
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep bytes | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep start | awk -F':' '{print $2" "$3}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep stop | awk -F':' '{print $2" "$3}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep sample | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep possible | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep name | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep code | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep latitude | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep longitude | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep elevation | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "satellites w/ obs" | awk -F':' '{print $2}' 2> /dev/null
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "Complete observations" | awk -F':' '{print $2}' 2> /dev/null
 
 
 done
