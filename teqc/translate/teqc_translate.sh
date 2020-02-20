9475d4c66aaaeb87c310af1ee4b32a80d78038f7
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:29:56 -0500

    teqc translate v4

--- teqc/translate/teqc_translate.sh
+++ teqc/translate/teqc_translate.sh
@@ -8,11 +8,13 @@ basepath="/mnt/f/Sask2018/GNSS1/"
 #basepath= "/mnt/f/PAD_GNSS"
 for f in "$basepath""$1"/"$2"/*/*.18_
 do
-	echo $f | awk -F'/' '{print $8"/"$9}'>>filepath_var.txt
-	filepath=cat filepath_var.txt
-	mkdir -p "$basepath"rinex/"$1"/"$2"/"$dirpath1"
-	#/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
-	#echo $filepath
+        echo $f | awk -F'/' '{print $8"/"$9}' > name.txt
+        filepath="$(cat name.txt)"
+        echo $f | awk -F'/' '{print $8}' > folder.txt
+        dirpath="$(cat folder.txt)"
+        mkdir -p "$basepath"rinex/"$1"/"$2"/"$dirpath"
+        #/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
+        #echo $filepath
 done
-#rm filepath_var.txt
+rm name.txt folder.txt
 cd $wd
