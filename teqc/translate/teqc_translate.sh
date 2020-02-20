fd048e46f052ffddb6eab3c83dea40c75f173aff
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:29:41 -0500

    teqc translate v3

--- teqc/translate/teqc_translate.sh
+++ teqc/translate/teqc_translate.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 # first arg is Receiver SSID, second arg is log: (LOG2_B or LOG5_hi)
+# version 3 includes stderr output file; also puts rinex in sep folder
 
 wd="$(pwd)"
 c=1
-for f in ls /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/*/*.18_
+basepath="/mnt/f/Sask2018/GNSS1/"
+#basepath= "/mnt/f/PAD_GNSS"
+for f in "$basepath""$1"/"$2"/*/*.18_
 do
-	echo $f | awk -F'/' '{print $8"/"$9}' > name.txt
-	filepath="$(cat name.txt)"
-	echo $f | awk -F'/' '{print $8}' > folder.txt
-	dirpath="$(cat folder.txt)"
-	#cd $f
-	#filepath=
-	#filepath="$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$(f)" | grep filename | awk -F'/' '{print $3"/"$4}')"
-	#dirpath="$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$(f)" | grep filename | awk -F'/' '{print $3}')"
-	mkdir -p /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$dirpath"
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
+	echo $f | awk -F'/' '{print $8"/"$9}'>>filepath_var.txt
+	filepath=cat filepath_var.txt
+	mkdir -p "$basepath"rinex/"$1"/"$2"/"$dirpath1"
+	#/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
 	#echo $filepath
-#	touch /mnt/f/SWOT2018/GNSS1/"$1"/"$2"//rinex/"$filepath".test
-#	echo /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".test
 done
+#rm filepath_var.txt
 cd $wd
