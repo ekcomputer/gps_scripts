081e7dad398c5d0c1654b98f2d9bef962d164efa
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:24:01 -0500

    teqc mine v7

--- teqc/mine/teqc_mine.sh
+++ teqc/mine/teqc_mine.sh
@@ -1,21 +1,27 @@
 #!bin/bash
 # a script to mine  filename, size, start/stop, sample interval, possible missing epochs, station name, station code, lat/long/elev, sats
 
+# V7 reads from SBF format, not rinex.
 # V6 includes receiver SSID and DOY
 # V5 saves to vars 
 
-searchpath="/mnt/f/Sask2018/GNSS1" # name of directory w data
+searchpath="$1" # name of directory w data
 echo -e "Starting mining script for: ""$1""\n\n"
 logs="$searchpath"/rinex/stats
 #rm "$logs"/*.txt
 echo $logs
-echo "RinexFilename,Bytes,ReceiverSSID,StartDateTime,StopDateTime,CommonDate,DOY,NumSamples,PossMissEpochs,StationName,StationCode,Latitude,Longitude,Elevation,NumSats,CompleteObs" > $logs/stats.csv
-for f in "$searchpath"/rinex/*/*/*/*.dat
+echo "RinexFilename,Bytes,ReceiverSSID,StartDateTime,StopDateTime,CommonDate,DOY,SamplePeriod,PossMissEpochs,StationName,StationCode,Latitude,Longitude,Elevation,NumSats,CompleteObs,SBF_Filename, SBF_bytes" > $logs/stats.csv
+for g in "$searchpath"/rinex/*/*/*/*.dat # g is rinex filename, f is SBF file
 do
+	pathBase=$(echo $g | awk -F'rinex/' '{print $1 }')
+	pathEnd=$(echo $g | awk -F'rinex/' '{print $2 }')
+	pathEndSBF=$(awk -v var="$pathEnd" 'BEGIN {gsub(".dat", ".18_", var); print var }')
+	f="$pathBase""$pathEndSBF"
 	echo "Processing: ""$f"
 	filename=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep filename | awk -F':' '{gsub(/ /, "", $2); print $2}')
 #	echo "Filename: "$filename
-	bytes=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep bytes | awk -F':' '{gsub(/ /, "", $2); print $2}')
+	bytes=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$g" 2> /dev/null | grep bytes | awk -F':' '{gsub(/ /, "", $2); print $2}')
+	SBF_bytes=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep bytes | awk -F':' '{gsub(/ /, "", $2); print $2}')
 	start=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null | grep start | awk -F' ' '{gsub(/ /, "", $5); gsub(/ /, "", $6); print $5" "$6}')
 	stop=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep final | awk -F' ' '{gsub(/ /, "", $5); print $5" "$6}')
 	sample=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep sample | awk -F':' '{gsub(/ /, "", $2); print $2}')
@@ -25,14 +31,15 @@ do
 	lat=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep latitude | awk -F':' '{gsub(/ /, "", $2); print $2}')
 	long=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep longitude | awk -F':' '{gsub(/ /, "", $2); print $2}')
 	elev=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$f" 2> /dev/null| grep elevation | awk -F':' '{gsub(/ /, "", $2); print $2}')
-	sats=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "satellites w/ obs" | awk -F':' '{gsub(/ /, "", $2); print $2}')
-	completeObs=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$f" 2> /dev/null| grep "Complete observations" | awk -F':' '{gsub(/ /, "", $2); print $2}')
-	receiver=$(echo $f | awk -F'/' '{print $7}')
-	doyLong=$(echo $f | awk -F'/' '{print $9}')
+	sats=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$g" 2> /dev/null| grep "satellites w/ obs" | awk -F':' '{gsub(/ /, "", $2); print $2}')
+	completeObs=$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +qc "$g" 2> /dev/null| grep "Complete observations" | awk -F':' '{gsub(/ /, "", $2); print $2}')
+	receiver=$(echo $pathEnd | awk -F'/' '{print $1}')
+	doyLong=$(echo $pathEnd | awk -F'/' '{print $3}')
+	#echo $pathEnd
+	#echo "Daylong: ""$dayLong"
 	doy=$(($doyLong - 18000))
-	commonDate=$(echo $start |  awk -F' ' '{print $1}')	
-	#echo $doyLong
-	echo $filename,$bytes,$receiver,$start,$stop,$commonDate,$doy,$sample,$possMissEpochs,$stationName,$stationCode,$lat,$long,$elev,$sats,$completeObs >> $logs/stats.csv
+	commonDate=$(echo $start |  awk -F' ' '{print $1}')
+	echo $g,$bytes,$receiver,$start,$stop,$commonDate,$doy,$sample,$possMissEpochs,$stationName,$stationCode,$lat,$long,$elev,$sats,$completeObs,$filename,$SBF_bytes >> $logs/stats.csv
 
 done
 echo "Finished batch mining."
