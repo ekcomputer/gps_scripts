028daa342b896b148e7047ae3b8f26d78dd6200e
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:29:19 -0500

    teqc translate v2

--- teqc/translate/teqc_translate.sh
+++ teqc/translate/teqc_translate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# first arg is Receiver SSID, second arg is log: (LOG2_B or LOG5_hi)
 
-# This script converts septentrio binary format files (.18_) to rinex (.dat)
-SSID=$1
 wd="$(pwd)"
 c=1
-for f in ls /mnt/f/SWOT2018/GNSS1/"$SSID"/LOG2_B/*/*.18_
+for f in ls /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/*/*.18_
 do
 	echo $f | awk -F'/' '{print $8"/"$9}' > name.txt
 	filepath="$(cat name.txt)"
@@ -14,10 +13,10 @@ do
 	#filepath=
 	#filepath="$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$(f)" | grep filename | awk -F'/' '{print $3"/"$4}')"
 	#dirpath="$(/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc +meta "$(f)" | grep filename | awk -F'/' '{print $3}')"
-	mkdir -p /mnt/f/SWOT2018/GNSS1/"$1"/LOG2_B/rinex/"$dirpath"
-	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/LOG2_B/rinex/"$filepath".dat
+	mkdir -p /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$dirpath"
+	/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
 	#echo $filepath
-#	touch /mnt/f/SWOT2018/GNSS1/"$1"/LOG2_B/rinex/"$filepath".test
-#	echo /mnt/f/SWOT2018/GNSS1/"$1"/LOG2_B/rinex/"$filepath".test
+#	touch /mnt/f/SWOT2018/GNSS1/"$1"/"$2"//rinex/"$filepath".test
+#	echo /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".test
 done
 cd $wd
