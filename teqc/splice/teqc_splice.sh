288bd3395460694214a255f448a137496f02d982
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:26:55 -0500

    teqc splice v2

--- teqc/splice/teqc_splice.sh
+++ teqc/splice/teqc_splice.sh
@@ -1,29 +1,40 @@
 #!bin/bash
 # a script to splice all rinex files in a directory and output to the same directory
-# also adds in marker name (first input)
-# also converts to ...
+# first input is directoy (i.e. 3012375)
+# also adds in marker name (second input)
+# also converts to file with 'M' flag for mixed observations, in case lacking
+# TODO:  Add git control
 
 teqc='/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc'
-dir_in=$(pwd)
+root_dir=/mnt/f/PAD_GNSS/PAD_GNSS_Spliced/$1/
+for dir_in in "$root_dir"*/ #"$root_dir"*/ # g directory for collection day
+do
 	echo "directory: "$dir_in
 	date=$(echo $dir_in | awk -F'/' '{print $7 }') #common date, not DOY
 	#echo $date
-	h=$(ls -t "$dir_in"/GPS_only/*.dat.rnx | tail -1) # sample file ID base name (oldest file in dir.)
-	j=$(echo $h | awk -F'/' '{print $9 }') # take only local path
+	h=$(ls -t "$dir_in"*.dat | tail -1) # sample file ID base name (oldest file in dir.)
+	#echo "h="$h
+	j=$(echo $h | awk -F'/' '{print $8 }') # take only local path
 	#echo "J="$j
 	fileBase="${j:0:9}" 
-	#echo $h
+	#echo $fileBase
 	spliceName=$(echo $fileBase"_"$date"D.dat")
-	echo $sp
-	rm $g$spliceName # remove existing file before splicing!
-	mkdir "$dir_in"/GPS_only/
-        for j in LOG*.dat
-        do
-                $teqc -O.s G -R $j > "$dir_in"/GPS_only/$j.rnx
-        done
+	echo "removing old file: "$dir_in$spliceName
+	rm -f "$dir_in""$spliceName" # remove existing file before splicing!
+	rm -fr "$dir_in"mixed/
+	mkdir -p "$dir_in"mixed/
+		for j in "$dir_in"LOG*.dat
+		do	
+				end=$(echo $j | awk -F'/' '{print $8 }')
+				#echo "end: "$end
+				mixed_name=$(echo "$dir_in"mixed/"$end".rnx)
+				#echo "mixed_name: "$mixed_name
+				$teqc -O.s M $j > $mixed_name
+		done
 	echo "Creating file: "$spliceName
-	$teqc -O.at SEPPOLANT_X_MF -O.mo "\""$1"\"" $g*.dat > $g$spliceName
+	$teqc -O.at SEPPOLANT_X_MF -O.mo "\""$2"\"" "$dir_in"mixed/*.rnx > $dir_in$spliceName
 	#compressedName=${spliceName:0:19}".tar"
 	#echo "Compressing: "$compressedName
 	#tar -czf $g$compressedName $g$spliceName
 	echo ""
+done
