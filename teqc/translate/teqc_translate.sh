7b8c7314ee689ce4ff8fb63fc218b1afc62d8ac0
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 09:30:45 -0500

    teqc translate v5

--- teqc/translate/teqc_translate.sh
+++ teqc/translate/teqc_translate.sh
@@ -8,13 +8,18 @@ basepath="/mnt/f/Sask2018/GNSS1/"
 #basepath= "/mnt/f/PAD_GNSS"
 for f in "$basepath""$1"/"$2"/*/*.18_
 do
+	echo "Processing: "$f
         echo $f | awk -F'/' '{print $8"/"$9}' > name.txt
         filepath="$(cat name.txt)"
-        echo $f | awk -F'/' '{print $8}' > folder.txt
+	echo $filepath | awk -F'.' '{print $1}' > filepath2.txt
+        filepath2="$(cat filepath2.txt)"
+	#echo $filepath2
+	echo $f | awk -F'/' '{print $8}' > folder.txt
         dirpath="$(cat folder.txt)"
         mkdir -p "$basepath"rinex/"$1"/"$2"/"$dirpath"
-        #/mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > /mnt/f/SWOT2018/GNSS1/"$1"/"$2"/rinex/"$filepath".dat
+	mkdir -p "$basepath"rinex/log/"$1"/"$2"/"$dirpath"
+        /mnt/d/wslhome/teqc_CentOSLx86_64d/teqc "$f" > "$basepath"rinex/"$1"/"$2"/"$filepath2".dat 2> "$basepath"rinex/log/"$1"/"$2"/"$filepath2".txt
         #echo $filepath
 done
-rm name.txt folder.txt
+rm name.txt folder.txt filepath2.txt
 cd $wd
