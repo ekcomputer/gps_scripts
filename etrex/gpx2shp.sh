319ce48001f0a4d18d3336bd7f568c426e8b91c8
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 10:19:58 -0500

    gpx2shp v2

--- etrex/gpx2shp.sh
+++ etrex/gpx2shp.sh
@@ -1,15 +1,16 @@
 # converts gpx files to one shpfile using ogr2ogr
 # first input is dir of gpx files
+# version 2 is for PAD 2018!
 # bs_dir_out="/mnt/d/AboveData/Shorelines/shp/"
 # bs_dir_in="/mnt/d/AboveData/Shorelines/"
 
-bs_dir_in='/mnt/f/PAD2018/etrex_data/raw/'
-bs_dir_out="/mnt/f/PAD2018/etrex_data/raw/shp/UNC 2/"
-#for file in $bs_dir_in$1/*/Track*.gpx
-for file in "$bs_dir_in"Track*.gpx
+bs_dir_in="/mnt/f/PAD2018/etrex_data/raw/"
+bs_dir_out="/mnt/f/PAD2018/etrex_data/shp/"
+echo Yo $bs_dir_in$1/*/Track*.gpx
+for file in $bs_dir_in$1/*/*.gpx
 do 
 	echo file: $file
-	filebase=$(echo $file | awk -F'.' '{print $1 }' | awk -F'/' '{print $8}' | awk '{gsub(/ /, "", $1); print $1$2$3$4}')
+	filebase="$(echo $file | awk -F'.' '{print $1 }' | awk -F'/' '{print $NF}' | awk '{gsub(/ /, "", $1); print $1$2$3$4}')"
 	echo filebase: $filebase
 	pth_out="$bs_dir_out$1/$filebase.shp"
 	echo pth_out: $pth_out
