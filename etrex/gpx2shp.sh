c0b071b1df4dffbad8981f6ba60268ab28d30dd4
ethankyzivat <ethan_kyzivat@brown.edu>
2020-02-20 10:20:13 -0500

    gpx2shp v3

--- etrex/gpx2shp.sh
+++ etrex/gpx2shp.sh
@@ -1,19 +1,24 @@
 # converts gpx files to one shpfile using ogr2ogr
-# first input is dir of gpx files
-# version 2 is for PAD 2018!
-# bs_dir_out="/mnt/d/AboveData/Shorelines/shp/"
-# bs_dir_in="/mnt/d/AboveData/Shorelines/"
+# first input is relative path for dir of gpx files
+# second and third are input and output top dirs
+# version 3 takes dirs as input
+# put / at end of dir pathnames!
 
-bs_dir_in="/mnt/f/PAD2018/etrex_data/raw/"
-bs_dir_out="/mnt/f/PAD2018/etrex_data/shp/"
+# NOTE: you will need to delete empty files created if the .gpx only held tracks, and you set it to oupt waypoints, etc.
+# NOTE 2: you need to specify "tracks" etc at end of second argument, in addition to third arg
+# example: gpx2shp_3.sh all_temp /mnt/f/PAD2019/etrex/UMASS/ /mnt/f/PAD2019/etrex/UMASS/shp_tmp/waypoints/ waypoints
+
+bs_dir_in=$2
+bs_dir_out=$3
+gpx_type=$4 # either track_points, tracks, waypoints, or routes (rarely used) // if using multiple, put in quotes
 echo Yo $bs_dir_in$1/*/Track*.gpx
-for file in $bs_dir_in$1/*/*.gpx
-do 
+for file in $bs_dir_in$1/*.gpx
+do
 	echo file: $file
 	filebase="$(echo $file | awk -F'.' '{print $1 }' | awk -F'/' '{print $NF}' | awk '{gsub(/ /, "", $1); print $1$2$3$4}')"
 	echo filebase: $filebase
 	pth_out="$bs_dir_out$1/$filebase.shp"
 	echo pth_out: $pth_out
 	mkdir -p $bs_dir_out$1
-	ogr2ogr -fieldTypeToString DateTime "$pth_out" "$file" track_points
+	ogr2ogr -fieldTypeToString DateTime "$pth_out" "$file" $gpx_type
 done
